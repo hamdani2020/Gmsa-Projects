ecfe5e4040bbfc9f267bcd560cf29032f9f6a966
lusitechmall <hamdanialhassangandi2020@gmail.com>
2022-12-12 08:08:03 +0000

    first commit

---
+++ Gmsa_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GmsaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Gmsa_app'
---
+++ README.md
@@ -0,0 +1,1 @@
+# Gmsa-Projects
