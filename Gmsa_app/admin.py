ab8e683bdfd8cec9ae6567580058f9887868fe4b
Ali Arafat Yassin <aliarafatyassin@gmail.com>
2022-12-17 10:33:31 +0000

    f_back

--- Gmsa_app/admin.py
+++ Gmsa_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+class HadithAdmin(admin.ModelAdmin):
+    list_display = ["title","body","author","date"]
+
+admin.site.register(Hadith,HadithAdmin)
+
